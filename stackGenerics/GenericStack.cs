722a3828195b6ea840f985d6dcb0fb1ad9672c72
Raffaele <baroneraffaele97@hotmail.com>
2024-04-24 08:57:26 +0200

    Aggiungere i file di progetto.

---
+++ stackGenerics/GenericStack.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+public class GenericStack<T>
+{
+    private Stack<T> _stack;
+
+    public GenericStack()
+    {
+        _stack = new Stack<T>();
+    }
+
+    public void Push(T item)
+    {
+        _stack.Push(item);
+    }
+
+    public T Pop()
+    {
+        if (_stack.Count == 0)
+        {
+            throw new InvalidOperationException("Lo stack è vuoto");
+        }
+
+        return _stack.Pop();
+    }
+
+    public T Peek()
+    {
+        if (_stack.Count == 0)
+        {
+            throw new InvalidOperationException("Lo stack è vuoto");
+        }
+        return _stack.Peek();
+    }
+}
---
+++ stackGenerics/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stackGenerics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creazione di uno stack di interi
+            var intStack = new GenericStack<int>();
+            intStack.Push(1);
+            intStack.Push(2);
+            intStack.Push(3);
+            Console.WriteLine(intStack.Peek());  // Stampa: 3
+            Console.WriteLine(intStack.Pop());  // Stampa: 3
+
+            // Creazione di uno stack di stringhe
+            var stringStack = new GenericStack<string>();
+            stringStack.Push("Hello");
+            stringStack.Push("World");
+            Console.WriteLine(stringStack.Peek());  // Stampa: World
+            Console.WriteLine(stringStack.Pop());  // Stampa: World
+        }
+    }
+}
